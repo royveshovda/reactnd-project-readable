c7c19b51d144bcf588e35b5d99cffe36e951a820
Roy Veshovda <roy@veshovda.no>
2017-08-05 07:59:42 +0200

    feat: react-router

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import logo from '../logo.svg';
 import '../App.css';
 
@@ -13,6 +14,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Route exact path="/" render={() => (
+          <h2>ALL</h2>
+        )} />
+        <Route path="/category" render={() => (
+          <h2>CATEGORY</h2>
+        )} />
+        <Route path="/post" render={() => (
+          <h2>POST</h2>
+        )} />
       </div>
     );
   }
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux'
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
